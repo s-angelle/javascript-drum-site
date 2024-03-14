0cfd57d63617fcdcdd561af8db9273ab1d2cdcd5
Sabrina A <sabrinaangelle@gmail.com>
2024-03-14 18:03:54 +0900

    updated js file

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
 
+
+// Detecting Button Press
+
 const numberOfButtons = document.querySelectorAll('.drum').length;
 
 for (let i = 0; i < numberOfButtons; i++){
 
     document.querySelectorAll('.drum')[i].addEventListener('click', function(){
 
-        let audio = new Audio('sounds/tom-1.mp3');
+        const buttonInnerHTML = this.innerHTML;
 
-        switch (this.innerHTML){
-
-        case 'w':
-            audio = new Audio('sounds/crash.mp3');
-            audio.play();
-            break;
-
-        case 'a':
-            audio = new Audio('sounds/kick-bass.mp3');
-            audio.play();
-            break;
-
-        case 's':
-            audio = new Audio('sounds/snare.mp3');
-            audio.play();
-            break;
-
-        case 'd':
-            audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
-            break;
-
-        case 'j':
-            audio = new Audio('sounds/tom-2.mp3');
-            audio.play();
-            break;
-
-        case 'k':
-            audio = new Audio('sounds/tom-3.mp3');
-            audio.play();
-            break;
-
-        case 'l':
-            audio = new Audio('sounds/tom-4.mp3');
-            audio.play();
-            break;  
-            
-        default:
-            audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
-        }
-
-    });
+        makeSound(buttonInnerHTML);
+    
+    })
 }
 
 
+// Detecting keyboard presses
+
+document.addEventListener("keydown", function(event){
+
+    makeSound(event.key);
+
+})
+
+
+// Creating a function that will play a specific sound, depending on which key / button is pressed.
+
+makeSound = (key) => {
+
+    switch (key){
+
+        case 'w':
+            const crashSound = new Audio('sounds/crash.mp3');
+            crashSound.play();
+            break;
+
+        case 'a':
+           const kickBass  = new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+
+        case 's':
+            const snareSound = new Audio('sounds/snare.mp3');
+            snareSound.play();
+            break;
+
+        case 'd':
+            const tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case 'j':
+            const tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case 'k':
+            const tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case 'l':
+            const tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;  
+            
+        default:
+            let defaultAudio = new Audio('sounds/tom-1.mp3');
+            defaultAudio.play();
+        }
+
+}
