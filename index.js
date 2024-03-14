972d6508d2e3033000d1fe042bcdbff515ff3c11
Sabrina A <sabrinaangelle@gmail.com>
2024-03-14 11:43:23 +0900

    updated js file and css

--- index.js
+++ index.js
@@ -2,7 +2,54 @@
 const numberOfButtons = document.querySelectorAll('.drum').length;
 
 for (let i = 0; i < numberOfButtons; i++){
+
     document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-        alert('I got clicked!');
+
+        let audio = new Audio('sounds/tom-1.mp3');
+
+        switch (this.innerHTML){
+
+        case 'w':
+            audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+
+        case 'a':
+            audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+        case 's':
+            audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+
+        case 'd':
+            audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case 'j':
+            audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case 'k':
+            audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case 'l':
+            audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;  
+            
+        default:
+            audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+        }
+
     });
 }
+
+
